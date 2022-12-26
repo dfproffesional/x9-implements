36df415c525eb425e2a5f40ec5d1249d26688d09
Daniel F <dfproffesional@gmail.com>
2022-12-26 16:18:57 -0400

    🤔 Separate functionality

--- modules/ImageCashLetter/FileHeader.py
+++ modules/ImageCashLetter/FileHeader.py
@@ -2,60 +2,79 @@ import io
 from modules import read_json
 from pprint import pprint
 from config import MODULES_DIR
-import ebcdic
 
+# Directories and files
 PATH = MODULES_DIR+"/ImageCashLetter"
 STRUCTURE = read_json(PATH+"/X9Structure/X9.100-187-2008.json")['FileHeader']
+
+# Rules
 RULES = STRUCTURE["rules"]
 POSITIONS = STRUCTURE["positions"]
-DATA = read_json(PATH+"/X9Structure/example.local.json")['FileHeader']
+
+# Default Data:
+# Example: DATA = read_json(PATH+"/X9Structure/example.local.json")['FileHeader']
+DATA = {'recordType': None}
 OUTPUT = io.StringIO()
 
-def init():
-  try:
-    set_record_type()
-    write()
-    writeOnFileEncoded()
+def init()->None:
+  __set_record_type()
+  __write()
+  __write_on_file_encoded()
 
-  except Exception as err:
-    print(str(err))
+def set_data(data:dict, memory_info:str )->None:
+  global DATA,OUTPUT
+  DATA = {**DATA,**data}
+  OUTPUT = io.StringIO()
 
-def set_record_type():
+def set_structure(name:str)->None:
+  global STRUCTURE,RULES,POSITIONS
+  STRUCTURE = read_json(PATH+"/X9Structure/X9."+name+".json")['FileHeader']
+  RULES = STRUCTURE["rules"]
+  POSITIONS = STRUCTURE["positions"]
+
+def get_buffer_data()->str:
+  return OUTPUT.getvalue()
+
+def get_data()->dict:
+  return DATA
+
+def __set_record_type()->None:
   DATA['recordType'] = STRUCTURE['type']
 
-def set_parser(keyname:str):
-  size = get_dimension(POSITIONS[keyname])
-  justify = RULES[keyname]['align']
-
+def __set_parser(keyname:str):
+  size = __get_dimension(keyname)
+  justify = __get_align(keyname)
   if type(DATA[keyname]) == int:
     DATA[keyname] = str(DATA[keyname]).zfill(size)
   else:
     DATA[keyname] = DATA[keyname].ljust(size) if justify == "left" else DATA[keyname].rjust(size)
 
-def get_dimension(position:str)->int:
-  sep = position.split(":")
+def __get_align(keyname):
+  return RULES[keyname]['align']
+
+def __get_dimension(keyname:str)->int:
+  sep = POSITIONS[keyname].split(":")
   sep[0] = int(sep[0]) - 1
   sep[1] = int(sep[1])
   return sep[1] - sep[0]
 
-def write():
+def __write():
   for keyname in DATA:
-    validate_values(keyname)
-    set_parser(keyname)
-    writeOnBuffer(keyname)
+    __validate_values(keyname)
+    __set_parser(keyname)
+    __write_on_buffer(keyname)
 
-def writeOnBuffer(keyname):
+def __write_on_buffer(keyname):
   OUTPUT.write(DATA[keyname])
 
-def writeOnFileEncoded(filename='test.dat'):
+def __write_on_file_encoded(filename='test.dat'):
+  import ebcdic
   output = OUTPUT.getvalue()
   output = output.encode('cp1148')
   f = open(filename, "ab")
   f.write(output)
   f.close()
 
-def validate_values(keyname)->None:
+def __validate_values(keyname)->None:
   if(type(DATA[keyname]) != eval(RULES[keyname]['type'])):
     raise ValueError(RULES[keyname]['message'])
-
-init()
